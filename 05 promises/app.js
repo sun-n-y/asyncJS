62189b2b63d4d2b981375ff391d7f1a054585ce9
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-11 16:47:41 -0400

    add async js promises

---
+++ 05 promises/app.js
@@ -0,0 +1,20 @@
+const heading1 = document.querySelector('.one');
+const heading2 = document.querySelector('.two');
+const heading3 = document.querySelector('.three');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    heading1.style.color = 'red';
+    setTimeout(() => {
+      heading2.style.color = 'green';
+      setTimeout(() => {
+        heading3.style.color = 'blue';
+      }, 1000);
+    }, 1000);
+  }, 1000);
+});
+
+console.log('im an second');
+
+console.log(window);

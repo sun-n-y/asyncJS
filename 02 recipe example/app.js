388122a61d6a6cd055f71354f0f2c2aeb490088d
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-11 14:56:12 -0400

    add asyns js synchronous recipe example complete

--- 02 recipe example/app.js
+++ 02 recipe example/app.js
@@ -1,16 +1,20 @@
+//make soup
+//boil water 10 min
+//chop carrots
+//add carrots to boil for 5 min
+//chop onion
+//add onion to boil for 5 min
+
 //synchronous
-console.log(`I am first`);
-console.log(`I am second`);
-console.log(`I am third`);
+boilWater(10000);
+console.log(`chop onion`);
+boilWater(5000);
+console.log(`chop carrot`);
+boilWater(5000);
 
-console.log(`I am first`);
-boilingWater();
-console.log(`I am second`);
-console.log(`I am third`);
-
-function boilingWater() {
+function boilWater(time) {
   console.log('boiling....');
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < time; i++) {
     console.log('still not done...');
   }
   console.log('done.');

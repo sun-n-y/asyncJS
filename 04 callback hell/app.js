18c3cd63eabcfb9dc5ebeecc8026d95c1b77bf37
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-11 15:52:13 -0400

    add async js callback hell

---
+++ 04 callback hell/app.js
@@ -0,0 +1,17 @@
+//make soup
+//boil water 10 min
+//chop carrots
+//add carrots to boil for 5 min
+//chop onion
+//add onion to boil for 5 min
+
+boilWater(0);
+console.log('chop carrots');
+
+function boilWater(time) {
+  console.log('boiling....');
+  //browser takes the callack and runs it for the time,then comes back with it
+  setTimeout(() => {
+    console.log('done.');
+  }, time);
+}

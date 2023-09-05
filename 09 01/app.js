124b78a5d6b0d2e7f4df6eaf11b153d57d07f05d
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-05 14:28:58 -0400

    add async lesson 2 async behavior

--- 09 01/app.js
+++ 09 01/app.js
@@ -1,15 +1,11 @@
 //
 
 boilWater(10000);
-console.log(`chop carrot`);
-boilWater(5000);
-console.log(`chop onion`);
-boilWater(5000);
+console.log('chop carrots');
 
 function boilWater(time) {
   console.log('boiling...');
-  for (let i = 0; i < time; i++) {
-    console.log('still not done...');
-  }
-  console.log('done.');
+  setTimeout(() => {
+    console.log('done.');
+  }, time);
 }

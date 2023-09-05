ceb46a7520e47f59dfbaac6fe1d5bd017618c2b4
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-05 14:15:58 -0400

    add async lesson 1 recipe example of single threaded issues

---
+++ 09 01/app.js
@@ -0,0 +1,15 @@
+//
+
+boilWater(10000);
+console.log(`chop carrot`);
+boilWater(5000);
+console.log(`chop onion`);
+boilWater(5000);
+
+function boilWater(time) {
+  console.log('boiling...');
+  for (let i = 0; i < time; i++) {
+    console.log('still not done...');
+  }
+  console.log('done.');
+}
